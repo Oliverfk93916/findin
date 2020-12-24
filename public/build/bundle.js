69eb8397a6076f2e4aafe05ef852c9458beda626
Oliverfk93916 <43701377+Oliverfk93916@users.noreply.github.com>
2020-12-24 21:13:02 +0000

    marker red

--- public/build/bundle.js
+++ public/build/bundle.js
@@ -2225,22 +2225,37 @@ var app = (function () {
 
     // import marker from '../../google/mapMarker'
 
+    let clicked = false;
 
     function mapCurrentLocation(){
     	let map = new google.maps.Map(document.getElementById('interactiveMap'), {
     				center: new google.maps.LatLng(lat,lng),
     				disableDefaultUI: true,
-    				zoom: 18,
+    				zoom: 17,
     				mapTypeId: google.maps.MapTypeId.ROADMAP,
     				styles: brownStyles,
-    			}
-    		);
+    	});
+
     	let marker = new google.maps.Marker({
     				position: new google.maps.LatLng(lat,lng),
     				map: map,
     				title: "You are Here",
-    				animation: google.maps.Animation.DROP
-    			});
+    				animation: google.maps.Animation.DROP,
+    				// icon: 'http://maps.google.com/mapfiles/ms/icons/orange-dot.png'
+
+    	});
+    	let info = new google.maps.InfoWindow({
+    				content: "Current Location"
+    	});
+
+    	google.maps.event.addListener(marker, "click", function(){
+    		clicked = ! clicked; 
+    		if (clicked == true) {
+    			info.open(map, marker);
+    		} else {
+    			info.close(map, marker);
+    		}
+    	});
 
     }
 
